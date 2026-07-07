f70a766568668d5a17044cfde46cb6900c1edd67
madhu786 <Bandarumadhu8@gmail.com>
2026-07-06 22:19:48 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TodayFit from "./TodayFit.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TodayFit />
+  </React.StrictMode>
+);
