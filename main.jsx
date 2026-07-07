5ca19985dc749e190efee2d7a57f815b5ded682f
madhu786 <Bandarumadhu8@gmail.com>
2026-07-06 22:18:11 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import TodayFit from "./TodayFit.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <TodayFit />
-  </React.StrictMode>
-);
